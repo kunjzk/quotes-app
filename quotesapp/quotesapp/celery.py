eee1724723abd2889d435034a67277250824b68e
Kunal Katarya <kunal.rox@gmail.com>
2025-10-12 17:15:06 +0800

    celery

---
+++ quotesapp/quotesapp/celery.py
@@ -0,0 +1,14 @@
+import os
+
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quotesapp.settings')
+
+app = Celery('quotesapp')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
